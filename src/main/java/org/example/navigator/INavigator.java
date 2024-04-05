df820601135cd9f942bbc31decd2ec527763df6e
Viktar Sharepa <shedelok.1@gmail.com>
2024-04-05 22:14:36 +0100

    Implement Navigator MVP

--- src/main/java/org/example/navigator/INavigator.java
+++ src/main/java/org/example/navigator/INavigator.java
@@ -5,12 +5,13 @@ import org.example.model.tetris.IPlanet;
 import org.example.model.tetris.IShipBaggage;
 
 import java.util.List;
+import java.util.Map;
 
 // Хранит IPlanet объекты, чтобы решать куда лететь
 public interface INavigator {
     // Возвращает ход перемещения, который сейчас надо сделать с заданным имеющимся багажом
-    List<String> getMove(IShipBaggage baggage);
+    List<String> getMove(String currentPlanet, IShipBaggage baggage);
 
     // Обновляет информацию о том какой мусор лежит на планете и возвращает ее
-    IPlanet setPlanetGarbage(String planet, List<Figure> garbage);
+    IPlanet setPlanetGarbage(String planet, Map<String, Figure> garbage);
 }
--- src/main/java/org/example/navigator/Navigator.kt
+++ src/main/java/org/example/navigator/Navigator.kt
@@ -4,15 +4,51 @@ import org.example.model.Figure
 import org.example.model.graph.Graph
 import org.example.model.tetris.IPlanet
 import org.example.model.tetris.IShipBaggage
+import org.example.model.tetris.TPlanet
+
+private const val EDEN = "Eden"
 
 class Navigator(graph: Graph) : INavigator {
+    private val nextOnThePathToEden = computeNextPlanetOnThePathToEden(graph)
+    private val planetNames = nextOnThePathToEden.keys
 
+    /*
+    Хранит только планеты, на которых точно знает весь мусор, не хранит Землю и Eden
+    */
+    private val knownPlanets = mutableMapOf<String, IPlanet>()
 
-    override fun getMove(baggage: IShipBaggage): List<String> {
-        TODO("Not yet implemented")
+    // едем на планету, про которую ничего не знаем?
+    private var willExplore = true
+
+    private fun buildPathToEden(origin: String): List<String> = buildList {
+        var planet = origin
+        while (planet != EDEN) {
+            planet = nextOnThePathToEden[planet]!!
+            add(planet)
+        }
     }
 
-    override fun setPlanetGarbage(planet: String, garbage: List<Figure>): IPlanet {
-        TODO("Not yet implemented")
+
+    private fun buildPath(origin: String, destination: String): List<String> {
+        return buildPathToEden(origin) + buildPathToEden(destination).reversed()
+    }
+
+    override fun getMove(currentPlanet: String, baggage: IShipBaggage): List<String> {
+        if (willExplore || knownPlanets.isEmpty()) {
+            val unexploredPlanet = planetNames.firstOrNull { it !in knownPlanets }
+            if (unexploredPlanet != null) {
+                willExplore = false
+                return buildPath(currentPlanet, unexploredPlanet)
+            }
+        }
+
+        return buildPath(currentPlanet, knownPlanets.maxBy { it.value.getHowManyCanAdd(baggage) }.key)
+    }
+
+    override fun setPlanetGarbage(planetName: String, garbage: Map<String, Figure>): IPlanet {
+        val planet = knownPlanets.getOrDefault(planetName, TPlanet())
+        planet.garbage = garbage
+        knownPlanets[planetName] = planet
+        return planet
     }
 }
