ca64685c7146e66888c49dd7098b2b9cf4ec03f2
Viktar Sharepa <shedelok.1@gmail.com>
2024-04-05 18:22:25 +0100

    Add comments for IPlanet and IShipBaggage

--- src/main/java/org/example/model/tetris/IPlanet.java
+++ src/main/java/org/example/model/tetris/IPlanet.java
@@ -8,9 +8,12 @@ import java.util.List;
 public interface IPlanet {
     float calcScore(IShipBaggage baggage);
 
+    // Загружает корабль
     void makeOptimalLoad(IShipBaggage baggage);
 
+    // Обновляет информацию о мусоре на этой планете
     void setGarbage(List<Figure> garbage);
 
+    // Возвращает весь мусор на этой планете
     List<Figure> getGarbage();
 }
--- src/main/java/org/example/model/tetris/IShipBaggage.java
+++ src/main/java/org/example/model/tetris/IShipBaggage.java
@@ -6,7 +6,9 @@ import org.example.model.PlacedFigure;
 import java.util.List;
 
 public interface IShipBaggage {
+    // Возвращает расположение мусора внутри корабля
     List<PlacedFigure> getLoad();
 
+    // Полностью очищает мусор внутри корабля
     void clean();
 }
