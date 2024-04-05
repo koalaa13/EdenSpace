2d99b6955c3b996e0fbd90780727bbfb63cdf2a5
Viktar Sharepa <shedelok.1@gmail.com>
2024-04-05 20:09:48 +0100

    Finish fake move implementation

--- src/main/java/org/example/api/fake/FivePlanetsBambooFakeJson.kt
+++ src/main/java/org/example/api/fake/FivePlanetsBambooFakeJson.kt
@@ -2,6 +2,7 @@ package org.example.api.fake
 
 import org.example.api.GameInfo
 import org.example.api.IJson
+import org.example.model.Figure
 import org.example.model.PlacedFigure
 import org.example.model.PlanetInfo
 import org.example.model.graph.Graph
@@ -16,6 +17,10 @@ private const val EDGE_WEIGHT = 1
 private const val CAPACITY_X = 1
 private const val CAPACITY_Y = 1
 
+private const val SHIP = "Ship"
+
+private val FIGURE = listOf(listOf(1, 1))
+
 /*
 Eden <-> Earth <-> One <-> Two <-> Three
 */
@@ -27,10 +32,26 @@ private fun getInitialMap(): Map<String, MutableMap<String, Int>> = mapOf(
     THREE to mutableMapOf(TWO to EDGE_WEIGHT)
 )
 
+/*
+from_one on One, from_two on Two, from_three on Three
+*/
+// Planet name, SHIP or null
+private fun getInitialItemPositions(): MutableMap<String, String?> = mutableMapOf(
+    "from_one" to ONE,
+    "from_two" to TWO,
+    "from_three" to THREE
+)
+
+
 class FivePlanetsBambooFakeJson : IJson {
     private val graph = getInitialMap()
+    private val itemPositions = getInitialItemPositions()
     private var currentPlanet = EARTH
     private var score = 0
+
+    private fun getFiguresOn(planetName: String): List<String> {
+        return itemPositions.entries.filter { it.value == planetName }.map { it.key }
+    }
 
     override fun getGameInfo(): GameInfo {
         val graph = Graph().apply {
@@ -50,13 +71,24 @@ class FivePlanetsBambooFakeJson : IJson {
             require(to in graph[currentPlanet]!!)
             currentPlanet = to
             if (currentPlanet == EDEN) {
-                score += 0
-                TODO("add score correctly")
-                TODO("clean ship")
+                itemPositions.iterator().run {
+                    while (hasNext()) {
+                        val itemPosition = next()
+                        if (itemPosition.value == SHIP) {
+                            remove()
+                            score++
+                        }
+                    }
+                }
             }
         }
 
-        return TODO()
+        return PlanetInfo().apply {
+            name = currentPlanet
+            garbage = getFiguresOn(currentPlanet).associate { figureName ->
+                figureName to Figure(FIGURE, figureName)
+            }
+        }
     }
 
     override fun load(newGarbage: List<PlacedFigure>) {
