dff83e3d7a439e35982aeaf5fa62bb8b332c4779
Viktar Sharepa <shedelok.1@gmail.com>
2024-04-06 01:23:50 +0100

    Make test in Fake less trivial

--- src/main/java/org/example/api/fake/FivePlanetsBambooFakeJson.kt
+++ src/main/java/org/example/api/fake/FivePlanetsBambooFakeJson.kt
@@ -15,12 +15,16 @@ private const val ONE = "One"
 private const val TWO = "Two"
 private const val THREE = "Three"
 private const val EDGE_WEIGHT = 1
-private const val CAPACITY_X = 1
-private const val CAPACITY_Y = 1
+private const val CAPACITY_X = 3
+private const val CAPACITY_Y = 3
 
 private const val SHIP = "Ship"
 
-private val FIGURE = listOf(listOf(1, 1))
+private data class ServerFigureInfo(
+    val shape: List<List<Int>>,
+    // Planet name, SHIP or null
+    var location: String,
+)
 
 /*
 Eden <-> Earth <-> One <-> Two <-> Three
@@ -33,26 +37,35 @@ private fun getInitialMap(): Map<String, MutableMap<String, Int>> = mapOf(
     THREE to mutableMapOf(TWO to EDGE_WEIGHT)
 )
 
-/*
-from_one on One, from_two on Two, from_three on Three
-*/
-// Planet name, SHIP or null
-private fun getInitialFigurePositions(): MutableMap<String, String?> = mutableMapOf(
-    "from_one" to ONE,
-    "from_two" to TWO,
-    "from_three" to THREE
-)
+private fun getInitialFigures(): MutableMap<String, ServerFigureInfo> = mutableMapOf<String, ServerFigureInfo>().apply {
+    val planets = setOf(ONE, TWO, THREE)
+    val shapes = listOf(
+        listOf(listOf(1, 1), listOf(1, 2), listOf(2, 2)),
+        listOf(listOf(1, 1), listOf(1, 2), listOf(2, 2)),
+        listOf(listOf(1, 1), listOf(1, 2), listOf(2, 2)),
+        listOf(listOf(1, 1), listOf(1, 2), listOf(2, 2)),
+
+        listOf(listOf(1, 1), listOf(1, 2), listOf(1, 3)),
+    )
+
+    for (planet in planets) {
+        for (i in shapes.indices) {
+            put("{$planet}_$i", ServerFigureInfo(shapes[i], planet))
+        }
+    }
+}
 
 private val shipVisualizer = ConsoleShipVisualizer()
 
 class FivePlanetsBambooFakeJson : IJson {
     private val graph = getInitialMap()
-    private val figurePositions = getInitialFigurePositions()
+
+    private val existingFigures = getInitialFigures()
     private var currentPlanet = EARTH
     private var score = 0
 
-    private fun getFiguresOn(planetName: String): List<String> {
-        return figurePositions.entries.filter { it.value == planetName }.map { it.key }
+    private fun getFiguresOn(planetName: String): List<Map.Entry<String, ServerFigureInfo>> {
+        return existingFigures.entries.filter { it.value.location == planetName }
     }
 
     override fun getGameInfo(): GameInfo {
@@ -68,17 +81,17 @@ class FivePlanetsBambooFakeJson : IJson {
     }
 
     override fun move(trajectory: List<String>): PlanetInfo {
-        println("[fake] move: ${trajectory.joinToString()}")
+        println("[fake] <score: $score> move: ${trajectory.joinToString()}")
 
         require(trajectory.isNotEmpty())
         for (to in trajectory) {
             require(to in graph[currentPlanet]!!)
             currentPlanet = to
             if (currentPlanet == EDEN) {
-                figurePositions.iterator().run {
+                existingFigures.iterator().run {
                     while (hasNext()) {
                         val figurePosition = next()
-                        if (figurePosition.value == SHIP) {
+                        if (figurePosition.value.location == SHIP) {
                             remove()
                             score++
                         }
@@ -89,26 +102,25 @@ class FivePlanetsBambooFakeJson : IJson {
 
         return PlanetInfo().apply {
             name = currentPlanet
-            garbage = getFiguresOn(currentPlanet).associate { figureName ->
-                figureName to Figure(FIGURE)
-            }
+            garbage = getFiguresOn(currentPlanet).associate { (name, info) -> name to Figure(info.shape) }
         }
     }
 
     override fun load(newGarbage: List<PlacedFigure>) {
-        println("[fake] load:")
+        println("[fake] <score: $score> load:")
         shipVisualizer.visualize(newGarbage, CAPACITY_X, CAPACITY_Y)
 
         require(currentPlanet != EDEN)
-        for (figureName in figurePositions.keys) {
-            if (figurePositions[figureName] == SHIP) {
-                figurePositions[figureName] = currentPlanet
+        for (figure in existingFigures.values) {
+            if (figure.location == SHIP) {
+                figure.location = currentPlanet
             }
         }
         for (figure in newGarbage) {
-            val name = figure.name
-            require(figurePositions[name]!! == currentPlanet)
-            figurePositions[name] = SHIP
+            val figureInfo = existingFigures[figure.name]
+            require(figureInfo != null)
+            require(figureInfo.location == currentPlanet)
+            figureInfo.location = SHIP
         }
     }
 }
